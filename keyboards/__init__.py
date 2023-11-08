71ae6e7f56501795a7cd9a3b913a4aa79f4c96f8
Natolin Artem <arnat64@yandex.ru>
2023-11-08 18:38:16 +0300

    add keyboardsfor statistic

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,15 +1,21 @@
-from .alias_keyboards import (add_alias_keyboard, alias_details_keyboard,  # noqa
-                              aliases_list_keyboard, aliases_menu_keyboard,
+from .alias_keyboards import (add_alias_keyboard,  # noqa
+                              alias_details_keyboard, aliases_list_keyboard,
+                              aliases_menu_keyboard,
                               confirm_del_alias_keyboard,
                               delete_alias_keyboard, get_alias_title_keyboard,
                               go_to_alias_keyboard)
-from .category_keyboards import (add_category_keyboard, categories_list_keyboard,  # noqa 
+from .category_keyboards import (add_category_keyboard,  # noqa
+                                 categories_list_keyboard,
                                  categories_menu_keyboard,
                                  category_details_keyboard,
                                  confirm_delete_category_keyboard,
                                  get_cat_title_keyboard,
                                  get_category_title_keyboard,
                                  go_to_new_category_keyboard)
+from .statistic_keyboards import (choose_interval_keyboard,  # noqa
+                                  confirm_mail_keyboard,
+                                  get_statistic_keyboard,
+                                  statistic_menu_keyboard)
 from .user_keyboards import (back_to_menu_keyboard, main_keyboard,  # noqa
                              other_keyboard, set_info_keyboard,
                              universal_keyboard)
---
+++ keyboards/statistic_keyboards.py
@@ -0,0 +1,118 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from utils.statistic import StatisticCallbackData, TimeInterval
+
+
+def statistic_menu_keyboard() -> InlineKeyboardMarkup:
+    """Основная клавиатура Статистики."""
+
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        InlineKeyboardButton(
+            text='За последний месяц',
+            callback_data=StatisticCallbackData(
+                action='statistic_intervals',
+                time_interval=TimeInterval.MONTH
+            ).pack()
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='За последний год',
+            callback_data=StatisticCallbackData(
+                action='statistic_intervals',
+                time_interval=TimeInterval.YEAR
+            ).pack()
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='За все время',
+            callback_data=StatisticCallbackData(
+                action='statistic_intervals',
+                time_interval=TimeInterval.TOTAL
+            ).pack()
+        ),
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='other'
+        )
+    )
+    return builder.as_markup()
+
+
+def choose_interval_keyboard() -> InlineKeyboardMarkup:
+    """Клавиатура выбора интревала расчета Статистики."""
+
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(
+            text='Получить в чат',
+            callback_data='send_statistic_to_chat'
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='Отправить на почту',
+            callback_data='confirm_mail'
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='statistic_menu'
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='В меню',
+            callback_data='other'
+        )
+    )
+    return builder.as_markup()
+
+
+def get_statistic_keyboard() -> InlineKeyboardMarkup:
+    """Клавиатура перехода после вывода Статистики."""
+
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='statistic_menu'
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='В меню',
+            callback_data='other'
+        )
+    )
+    return builder.as_markup()
+
+
+def confirm_mail_keyboard() -> InlineKeyboardMarkup:
+    """Клавиатура подтверждения почты перед отправкой сообщения."""
+
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(
+            text='Да',
+            callback_data='send_statistic_to_email'
+        ),
+        InlineKeyboardButton(
+            text='Поменять',
+            callback_data='get_mail'
+        )
+    )
+    builder.row(
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='statistic_menu'
+        )
+    )
+    return builder.as_markup()
