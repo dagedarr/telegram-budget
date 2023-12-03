c5b64e377b1db3d9db5f343ed7452e1005692be0
Natolin Artem <arnat64@yandex.ru>
2023-12-03 11:33:19 +0300

    added webhook wer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# ----------------------- telegram pooling ver -----------------------
+
 import asyncio
 import logging
 import sys
@@ -31,3 +33,69 @@ if __name__ == "__main__":
 
 # celery -A tasks.tasks:app worker --loglevel=INFO --pool=solo
 # celery -A tasks.tasks:app flower
+
+
+
+# ----------------------- telegram webhook ver for pythonanywhere -----------------------
+
+# import asyncio
+# import logging
+# import sys
+
+# from aiogram import Bot, Dispatcher, types
+# from aiogram.client.session.aiohttp import AiohttpSession
+# from aiogram.enums import ParseMode
+# from flask import Flask, request
+# from flask_sslify import SSLify
+
+# from config import Config
+# from core.db import AsyncSessionLocal
+# from core.db_middleware import DbSessionMiddleware
+# from handlers import handlers_router
+# from utils.middlewares import ChatClearMiddleware
+
+
+# dp = Dispatcher()
+
+# session = AiohttpSession(proxy="http://proxy.server:3128")
+# bot = Bot(
+#     token=Config.API_TOKEN,
+#     parse_mode=ParseMode.HTML,
+#     session=session
+# )
+
+# dp.update.middleware(ChatClearMiddleware())
+# dp.update.middleware(
+#     DbSessionMiddleware(session_pool=AsyncSessionLocal)
+# )
+# dp.include_routers(handlers_router)
+
+# app = Flask(__name__)
+# sslify = SSLify(app)
+
+
+# @app.route('/', methods=["POST", "GET"])
+# def bot_webhook():
+#     if request.method == 'POST':
+
+#         response = request.get_json()
+#         telegram_update = types.Update(**response)
+
+#         # just for pythonanywhere free plan
+#         # else dp.feed_update(bot=bot, update=telegram_update)
+#         loop = asyncio.get_event_loop()
+#         loop.run_until_complete(dp.feed_update(bot=bot, update=telegram_update))
+
+#         return 'OK', 200
+#     return 'Hello world!'
+
+
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+#     app.run(threaded=True)
+
+# # pip3 install flask flask-sslify aiohttp-socks
+
+# # you need to set webhook begore using:
+# # https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={YOUR_PYTHONANYWHERE_URL}
